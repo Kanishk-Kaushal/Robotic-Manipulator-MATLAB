a9acecfca6fa07bc1e69c1df4878e65d6715095b
Kanishk Kaushal <75947299+Kanishk-Kaushal@users.noreply.github.com>
2022-01-15 18:12:50 +0530

    Update Inverse_Kinematics_2_DOF.m

--- 2 DOF/Inverse Kinematics/Inverse_Kinematics_2_DOF.m
+++ 2 DOF/Inverse Kinematics/Inverse_Kinematics_2_DOF.m
@@ -3,28 +3,83 @@
 % Input Start 
 
 % Link Lengths
-L1 = 0.5;
+L1 = 1;
 L2 = 0.5;
 
 % Final End Effector Position
-Final_x = 0.5;
+Final_x = 1;
 Final_y = 0.5;
 
 % Input End
+
+% Desired End Effector Position Beyond Workspace Start
+
+% Maximum Extened Arm Length
+Arm_Radius = L1 + L2;
+
+% Arm Length Differnece 
+Arm_Diff = abs(L1 - L2);
+
+% End Effector Distance from Origin
+D = sqrt((Final_y*Final_y) + (Final_x*Final_x));
+
+% Outer Workspace Boundary
+if (D > Arm_Radius)
+    Final_x = (Arm_Radius)*(Final_x)/(D);
+    Final_y = (Arm_Radius)*(Final_y)/(D);
+end
+
+% Inner Workspace Boundary
+if(D < Arm_Diff)
+    Final_x = (Arm_Diff)*(Final_x)/(D);
+    Final_y = (Arm_Diff)*(Final_y)/(D);
+end
+
+% Desired End Effector Position Beyond Workspace End
+
+% Plotting the Workspace Start
+
+Theta_Circle = 0:pi/50:2*pi;
+
+% Plotting the Outer Workspace Boundaries
+X_outer_WS = (Arm_Radius*cos(Theta_Circle));
+Y_outer_WS = (Arm_Radius*sin(Theta_Circle));
+
+% Plotting the Inner Workspace Boundaries
+X_inner_WS = (Arm_Diff*cos(Theta_Circle));
+Y_inner_WS = (Arm_Diff*sin(Theta_Circle));
+
+% Plotting the Workspace End
 
 % Inverse Kinematics Calculation Start
 
 % End Effector State (R, Theta)
 R = sqrt((Final_y*Final_y) + (Final_x*Final_x));
-Theta = atan(Final_y/(Final_x + 0.00001));
+Theta = atan(Final_y/(Final_x + 0.000001));
+% 0.000001 is added to avoid Undefined Cartesian Points
 
 % Intermediate Angles
-Beta = acos(((R*R)-(L1*L1)-(L2*L2))/((-1)*2*L1*L2));
-Alpha = asin((L2*sin(Beta))/R);
+Beta = acos(((L1*L1) + (L2*L2) - (R*R))/(2*L1*L2));
+Alpha = acos(((L1*L1) + (R*R) - (L2*L2))/(2*L1*R));
+
+% 2nd Quadrant End Effector Position
+if (Final_x < 0)
+
+    Theta = Theta + pi;
+    Beta = (-1)*Beta;
+    Alpha = (-1)*Alpha;
+
+end
 
 % Final Joint Angles in Radians
-Final_Angle_1 = Theta - Alpha;
-Final_Angle_2 = pi - Beta;
+
+% Elbow UP Configuration
+Final_Angle_1 = (Theta + Alpha);
+Final_Angle_2 = (Beta - pi);
+
+% Elbow DOWN Configuration
+% Final_Angle_1 = (Theta - Alpha);
+% Final_Angle_2 = pi - Beta;
 
 % Final Joint Angles in Degrees
 Final_Angle_1 = Final_Angle_1*(180/pi);
@@ -32,7 +87,7 @@ Final_Angle_2 = Final_Angle_2*(180/pi);
 
 % Inverse Kinematics Calculation End
 
-% Angle with the horizontal (Degrees)
+% Angle with the Horizontal (Degrees)
 A1 = linspace(0, (Final_Angle_1), 10);
 A2 = linspace(0, (Final_Angle_2), 30);
 
@@ -70,6 +125,12 @@ for i = 1 : length(A1)
 
         % Plot
         plot([X0 X1], [Y0 Y1], [X1 X2], [Y1 Y2], 'linewidth', 5)
+        hold on
+        plot(X_inner_WS, Y_inner_WS);
+        plot(X_outer_WS, Y_outer_WS);
+        fill([X_inner_WS flip(X_outer_WS)],[Y_inner_WS flip(Y_outer_WS)],'g')
+        hold off
+        alpha(0.1)
         xlabel('X-Axis (m)')
         ylabel('Y-Axis (m)')
         title('Inverse Kinematics on a 2 DOF Robotic Manipulator');
@@ -78,19 +139,11 @@ for i = 1 : length(A1)
         text(X2, Y2, txtend, 'HorizontalAlignment', 'left', 'VerticalAlignment', 'bottom')
         text(0.5*(X0 + X1), 0.5*(Y0 + Y1), '  Link 1')
         text(0.5*(X1 + X2), 0.5*(Y1 + Y2), '  Link 2')
+        text(((-1)*Final_x), 0, 'Workspace')
         grid on
-        axis([-1 2 -0.5 2])
-        pause(0.3)
+        axis([-3 3 -3 3])
 
         M(counter) = getframe(gcf);
         counter = counter + 1;
     end
 end
-
-% Creating the Animation
-movie(M);
-videofile = VideoWriter('Inverse Kinematics on a 2 DOF Robotic Manipulator.avi', 'Uncompressed AVI');
-open(videofile);
-writeVideo(videofile, M);
-close(videofile);
-
