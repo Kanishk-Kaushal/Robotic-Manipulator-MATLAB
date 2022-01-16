dda79202aeba689493456fe14184bb8ee1689cff
Kanishk Kaushal <75947299+Kanishk-Kaushal@users.noreply.github.com>
2022-01-16 13:16:28 +0530

    Update Inverse_Kinematics_2_DOF.m

--- 2 DOF/Inverse Kinematics/Inverse_Kinematics_2_DOF.m
+++ 2 DOF/Inverse Kinematics/Inverse_Kinematics_2_DOF.m
@@ -4,10 +4,10 @@
 
 % Link Lengths
 L1 = 1;
-L2 = 0.5;
+L2 = 1;
 
 % Final End Effector Position
-Final_x = 1;
+Final_x = -1;
 Final_y = 0.5;
 
 % Input End
@@ -88,7 +88,7 @@ Final_Angle_2 = Final_Angle_2*(180/pi);
 % Inverse Kinematics Calculation End
 
 % Angle with the Horizontal (Degrees)
-A1 = linspace(0, (Final_Angle_1), 10);
+A1 = linspace(0, (Final_Angle_1), 30);
 A2 = linspace(0, (Final_Angle_2), 30);
 
 % Forward Kinematics:
@@ -96,54 +96,82 @@ A2 = linspace(0, (Final_Angle_2), 30);
 % Counter and Loop Initialization
 counter = 1;
 
+% Arrays that will stor End Effector Positions
+X_Arr = [];
+Y_Arr = [];
+
 for i = 1 : length(A1)
 
     Theta_1 = A1(i);
+    Theta_2 = A2(i);
+    
+    % Coordinates:
 
-    for j = 1 : length(A2)
+    % Initial points of Link 1
+    X0 = 0;
+    Y0 = 0;
 
-        Theta_2 = A2(j);
+    % Final points of Link 1 
+    X1 = L1*cosd(Theta_1);
+    Y1 = L1*sind(Theta_1);
+    
+    % Final points of Link 2
+    X2 = X1 + L2*cosd(Theta_1 + Theta_2);
+    Y2 = Y1 + L2*sind(Theta_1 + Theta_2);
+    
+    % Storing End Effector POsitions to Plot Trajectory
+    X_Arr(i) = X2;
+    Y_Arr(i) = Y2;
+
+    % Storing Initial End Effector Coordinates for Plotting Trajectory
+    if (counter == 1)
         
-        % Coordinates:
+        Eef_X_init = X2;
+        Eef_Y_init = Y2;
 
-        % Initial points of Link 1
-        X0 = 0;
-        Y0 = 0;
-
-        % Final points of Link 1 
-        X1 = L1*cosd(Theta_1);
-        Y1 = L1*sind(Theta_1);
-        
-        % FInal points of Link 2
-        X2 = X1 + L2*cosd(Theta_1 + Theta_2);
-        Y2 = Y1 + L2*sind(Theta_1 + Theta_2);
-
-        % Assigning the Parameters
-        txt1 = ['T1 = ', num2str(Theta_1), ' Deg'];
-        txt2 = ['T2 = ', num2str(Theta_2), ' Deg'];
-        txtend = ['X2 = ', num2str(X2), ', ', 'Y2 = ', num2str(Y2)];
-
-        % Plot
-        plot([X0 X1], [Y0 Y1], [X1 X2], [Y1 Y2], 'linewidth', 5)
-        hold on
-        plot(X_inner_WS, Y_inner_WS);
-        plot(X_outer_WS, Y_outer_WS);
-        fill([X_inner_WS flip(X_outer_WS)],[Y_inner_WS flip(Y_outer_WS)],'g')
-        hold off
-        alpha(0.1)
-        xlabel('X-Axis (m)')
-        ylabel('Y-Axis (m)')
-        title('Inverse Kinematics on a 2 DOF Robotic Manipulator');
-        text(X0, Y0, txt1, 'VerticalAlignment', 'top')
-        text(X1, Y1, txt2, 'VerticalAlignment', 'top')
-        text(X2, Y2, txtend, 'HorizontalAlignment', 'left', 'VerticalAlignment', 'bottom')
-        text(0.5*(X0 + X1), 0.5*(Y0 + Y1), '  Link 1')
-        text(0.5*(X1 + X2), 0.5*(Y1 + Y2), '  Link 2')
-        text(((-1)*Final_x), 0, 'Workspace')
-        grid on
-        axis([-3 3 -3 3])
-
-        M(counter) = getframe(gcf);
-        counter = counter + 1;
     end
+
+    % Assigning the Parameters
+    txt1 = ['T1 = ', num2str(Theta_1), ' Deg'];
+    txt2 = ['T2 = ', num2str(Theta_2), ' Deg'];
+    txtend = ['X2 = ', num2str(X2), ', ', 'Y2 = ', num2str(Y2)];
+
+    % Plot
+    plot([X0 X1], [Y0 Y1], [X1 X2], [Y1 Y2], 'linewidth', 5)
+
+    hold on
+
+    % Plots Displacement
+    plot([Eef_X_init X2], [Eef_Y_init Y2], '-*')
+    
+    % Plots Trajectory
+    plot(X_Arr, Y_Arr, 'o')
+
+    plot(X_inner_WS, Y_inner_WS);
+    plot(X_outer_WS, Y_outer_WS);
+
+    % Plots Workspace
+    fill([X_inner_WS flip(X_outer_WS)],[Y_inner_WS flip(Y_outer_WS)],'g')
+
+    hold off
+    
+    % Increases Transparency of Workspace
+    alpha(0.1)
+
+    xlabel('X-Axis (m)')
+    ylabel('Y-Axis (m)')
+    title('Inverse Kinematics on a 2 DOF Robotic Manipulator');
+    text(X0, Y0, txt1, 'VerticalAlignment', 'top')
+    text(X1, Y1, txt2, 'VerticalAlignment', 'top')
+    text(X2, Y2, txtend, 'HorizontalAlignment', 'left', 'VerticalAlignment', 'bottom')
+    text(0.5*(X0 + X1), 0.5*(Y0 + Y1), '  Link 1')
+    text(0.5*(X1 + X2), 0.5*(Y1 + Y2), '  Link 2')
+    text(((-1)*Final_x), 0, 'Workspace')
+    grid on
+
+    axis([-3 3 -3 3])
+
+    M(counter) = getframe(gcf);
+    counter = counter + 1;
+    
 end
